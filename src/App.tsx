1ea41ecf578655f2480a5ab9afd8d95d1b0f71f4
tatsu1109 <ts11252tt1109@gmail.com>
2021-03-14 01:02:00 +0900

    リストの保存先をindexedDB(Dexie)に置き換え

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,31 @@ import {
 } from "@material-ui/core";
 import { Delete, AddCircleOutline } from "@material-ui/icons";
 import axios from "axios";
+import Dexie, { Table } from "dexie";
+import { useLiveQuery } from "dexie-react-hooks";
+
+interface WordList {
+  word: string;
+  meaning: string;
+}
+
+class WordListDB extends Dexie {
+  wordList!: Table<WordList, string>;
+
+  constructor() {
+    super("wordListDB");
+    this.version(1).stores({
+      wordList: `
+        word,
+        meaning`,
+    });
+  }
+}
+
+const db = new WordListDB();
 
 const App = () => {
-  const [wordList, setWordList] = useState([
-    { word: "sample", meaning: "サンプル" },
-  ]);
+  const wordList = useLiveQuery(() => db.wordList.toArray());
   const [input, setInput] = useState("default");
   const [target, setTarget] = useState("");
 
@@ -34,10 +54,8 @@ const App = () => {
             },
           }
         );
-        setWordList((wordList) => [
-          ...wordList,
-          { word: target, meaning: res.data.message },
-        ]);
+        db.wordList?.add({ word: target, meaning: res.data.message });
+        // .catch((err) => console.error(err));
       };
       fetchData();
     }
@@ -46,21 +64,20 @@ const App = () => {
   return (
     <div>
       <Typography variant="h6">単語リスト</Typography>
+      <button onClick={() => db.wordList.clear()}>Clear</button>
       <List>
-        {wordList.map((word, index) => {
+        {wordList?.map((row, index) => {
           return (
             <ListItem key={index}>
-              <TextField defaultValue={word.word} />
-              <ListItemText primary={word.meaning} />
+              <TextField defaultValue={row.word} />
+              <ListItemText primary={row.meaning} />
               <ListItemSecondaryAction>
                 <IconButton
                   edge="end"
                   aria-label="delete"
-                  onClick={() =>
-                    setWordList(
-                      wordList.filter((_, targetIndex) => index !== targetIndex)
-                    )
-                  }
+                  onClick={() => {
+                    db.wordList.delete(row.word);
+                  }}
                 >
                   <Delete />
                 </IconButton>
